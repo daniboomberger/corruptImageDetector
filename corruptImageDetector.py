18bc18f3ccad7e18d21f1ab8ecc1c85f3a7a01b4
ACMT\D.Jakupi <d.jakupi@acommit.ch>
2020-08-21 09:09:28 +0200

    code clean up and changed way of comments

--- corruptImageDetector.py
+++ corruptImageDetector.py
@@ -1,33 +1,29 @@
 import os
 from PIL import Image
 
-picture_file_endings = ['*.jpg', '*.gif', '*.bmp', '*.png', '*.svg', '*.jpeg', '.*jif', '*.webp', '*.tiff', '*.raw']
+picture_file_endings = ['.jpg', '.gif', '.bmp', '.png', '.svg', '.jpeg', '.jif', '.webp', '.tiff', '.raw']
 
-'''
-function to get files of from inputed folder path
-'''
+
+#function to get files of from inputed folder path
 def get(path):
     files = os.listdir(path)
     for pic in files:
         if checkFileEnding(pic) == 1:
-            print(checkImg(pic))
+            checkImg(pic, path)
 
-'''
-checks with pillow libary if image is corrupt and prints corrupt file name out
-'''
-def checkImg(pic): 
+
+#checks with pillow libary if image is corrupt and prints corrupt file name out
+def checkImg(pic, path): 
     try: 
-        img = Image.open(pic)
+        img = Image.open(path + pic)
         img.verify()
     except:
-        count += count + 1
         print(pic)
 
-'''
-checks for image file endings
-'''
+
+#checks for image file endings
 def checkFileEnding(pic): 
-    for i in range(0, len(picture_file_ending) - 1): 
+    for i in range(0, len(picture_file_endings) - 1): 
         if picture_file_endings[i] in pic: 
             return 1
     return 0
